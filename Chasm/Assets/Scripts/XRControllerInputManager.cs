bd2244b827dff8b6e91f55b6d278b8c5e925f857
Riley Mahr <rwmahr@gmail.com>
2018-02-27 18:06:41 -0600

    project initialization and concept

---
+++ Chasm/Assets/Scripts/XRControllerInputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class XRControllerInputManager : MonoBehaviour {
+
+    [Header("Trigger and Grip inputs")]
+    [SerializeField] private string trigger;
+    [SerializeField] private string grip;
+
+    [Header("For Testing")]
+    [SerializeField] private GameObject hand;
+    [SerializeField] private Color none = Color.gray, triggerColor = Color.blue, gripColor = Color.cyan;
+    private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+        rend = hand.GetComponent<Renderer>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis(trigger) == 1.0)
+        {
+            rend.material.color = triggerColor;
+        } else if (Input.GetAxis(grip) == 1.0)
+        {
+            rend.material.color = gripColor;
+        } else
+        {
+            rend.material.color = none;
+        }
+    }
+}
